257ddd4083923d58be6a8bafc1a1481df9a9ae95
Ben Calabrese <bencalabrese@gmail.com>
2016-04-28 14:32:28 -0700

    spectacle modal style

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -1,9 +1,15 @@
 class Api::TicketsController < ApplicationController
   def update
     ids = params[:ticket_ids]
-    Ticket.where(id: ids).update_all(user_id: current_user.id)
 
-    @tickets = Ticket.where(id: ids)
+    @tickets = Ticket.where(id: ids, user_id: nil)
+
+    if @tickets.count != ids.count
+      @errors = ["Some of those seats have already been claimed"]
+      render "api/errors"
+    end
+
+    @tickets.update_all(user_id: current_user.id)
     render :update
   end
 end
--- frontend/components/spectacles/spectacle_modal.jsx
+++ frontend/components/spectacles/spectacle_modal.jsx
@@ -9,7 +9,20 @@ var SpectacleModal = React.createClass({
     return (
       <div className="spectacle-modal">
         <div className="spectacle-modal-content">
-          I'm an event modal for {spectacle.title}
+          <div className="spectacle-modal-picture-pane">
+            <img src={spectacle.image_url}/>
+            <button>Reserve Tickets</button>
+            <button>Find Other Shows</button>
+          </div>
+
+          <div className="spectacle-modal-details">
+            <h2>{spectacle.title}</h2>
+            <h6>The Curran Theater | 200+ seats</h6>
+            <h6>April 22nd 2016 - May 8th 2016</h6>
+            <p>
+              {spectacle.description}
+            </p>
+          </div>
         </div>
 
         <SpectacleModalNav/>
