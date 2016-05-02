612f270dec21432ace80b5fc1edf1550cc934899
Ben Calabrese <bencalabrese@gmail.com>
2016-05-02 11:50:35 -0700

    add temporary alerts to prompt login

--- frontend/mixins/set_cart_status.js
+++ frontend/mixins/set_cart_status.js
@@ -1,6 +1,9 @@
-var CartActions = require('../actions/cart_actions');
+var CartActions = require('../actions/cart_actions'),
+    CurrentUserStateMixin = require('./current_user_state');
 
 var SetCartStatus = {
+  mixins: [CurrentUserStateMixin],
+
   goToDetail: function(event) {
     event.preventDefault();
     CartActions.updateCartStatus("closed");
@@ -8,17 +11,29 @@ var SetCartStatus = {
 
   goToPerformances: function(event) {
     event.preventDefault();
-    CartActions.updateCartStatus("picking performance");
+    if (this.state.currentUser) {
+      CartActions.updateCartStatus("picking performance");
+    } else {
+      alert("Please login to book tickets");
+    }
   },
 
   goToSeats: function(event) {
     event.preventDefault();
-    CartActions.updateCartStatus("picking seats");
+    if (this.state.currentUser) {
+      CartActions.updateCartStatus("picking seats");
+    } else {
+      alert("Please login to book tickets");
+    }
   },
 
   goToConfirmation: function(event) {
     event.preventDefault();
-    CartActions.updateCartStatus("confirmation");
+    if (this.state.currentUser) {
+      CartActions.updateCartStatus("confirmation");
+    } else {
+      alert("Please login to book tickets");
+    }
   }
 };
 
