4b3f255a98a14b7cd3a9257abd15308a0912a450
Ben Calabrese <bencalabrese@gmail.com>
2016-05-06 11:28:42 -0700

    reconfigure webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,47 @@
+var path = require("path"),
+    webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/pickup_tix.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+  devtool: 'source-maps',
+};
